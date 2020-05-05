932f33c86c1802202afb71e115c9c49deabb2910
weiYiGe <weiYiGe@gmail.com>
2020-05-05 22:25:48 +0800

    第一次提交

---
+++ miniprogram/pages/loading/loading.js
@@ -0,0 +1,16 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    
+  },  
+  tapToIndex: function() {
+    wx.navigateTo({
+      url: '../index/index'
+    })
+  },
+  //事件处理函数
+  
+})
